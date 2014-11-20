05ee8aec8d40c6733be50b9f46f737bbd9f6b498
mizoki <h.mizoki@gmail.com>
2014-11-20 17:05:44 +0900

    Modify to use the ChatworkCliController class

--- chatwork-cli-get-rooms.rb
+++ chatwork-cli-get-rooms.rb
@@ -1,27 +1,8 @@
-require 'faraday'
-require 'json'
+require_relative './chatwork-cli-controller'
 
-BASE_URI='https://api.chatwork.com/v1/'
-API_TOKEN=ENV['CHATWORK_API_TOKEN']
-
-GET_ROOMS='rooms'
-
-chatwork_connection = Faraday::Connection.new(url: BASE_URI) do |builder|
-  builder.use Faraday::Request::UrlEncoded
-  builder.use Faraday::Response::Middleware
-  builder.use Faraday::Adapter::NetHttp
-end
-
-response = chatwork_connection.get do |request|
-  request.url GET_ROOMS
-  request.headers = {
-    'X-ChatWorkToken' => API_TOKEN
-  }
-end
-
-response.success? ? results = JSON.parse(response.body) : return
+chatwork = ChatworkCliController.new
+results = chatwork.get_unread_rooms
 
 results.each do |result|
-  puts result['name'] + ' : Unread [' + result['unread_num'].to_s + '] Mention [' + result['mention_num'].to_s + ']' unless result['unread_num'].zero?
+  puts result['name'] + ' : Unread [' + result['unread_num'].to_s + '] Mention [' + result['mention_num'].to_s + ']'
 end
-
