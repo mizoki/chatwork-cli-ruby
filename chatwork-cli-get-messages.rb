b3e1ee33636ab85fb4e4f47bbc93577db33765ed
mizoki <h.mizoki@gmail.com>
2014-11-28 14:07:34 +0900

    Add chatwork-cli-get-messages.rb

---
+++ chatwork-cli-get-messages.rb
@@ -0,0 +1,27 @@
+require_relative './controllers/chatwork-cli-controller'
+
+chatwork = ChatworkCliController.new
+rooms = chatwork.get_room_ids
+
+rooms.each_with_index do |room, i|
+  puts sprintf('%02d: %s', i+1, room['name'])
+end
+
+input_id = gets
+
+rooms.each_with_index do |room, i|
+  if (i+1) == input_id.to_i
+    puts room['name']
+    @results = chatwork.get_messages room['room_id']
+  end
+end
+
+puts '----------------------------------------------------------------------'
+
+@results.each_with_index do |res, i|
+  puts res['account']['name']
+  res['body'].split('\n').each do |line|
+    puts line
+  end
+  puts '----------------------------------------------------------------------'
+end
